c3d28347af8562d134ee92252601240f5df681fd
hs3082 <56650844+hs3082@users.noreply.github.com>
2020-08-04 17:18:00 -0400

    Add files via upload

---
+++ MemeMe v2/MemeMe v2/SentMemesCollectionViewController.swift
@@ -0,0 +1,16 @@
+//
+//  SentMemesCollectionViewController.swift
+//  MemeMe v1
+//
+//  Created by Howard Snyder on 7/28/20.
+//  Copyright © 2020 Howard Snyder. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+var memes: [Meme]! {
+    let object = UIApplication.shared.delegate
+    let appDelegate = object as! AppDelegate
+    return appDelegate.memes
+}
